0df0394027840bb33e0fe2d2245e32e30e7b61e5
加勒比海涛 <13555089837@163.com>
2020-12-30 22:52:03 +0800

    上传代码

---
+++ C语言刷题课/33-计算平均成绩.c
@@ -0,0 +1,44 @@
+//题目描述
+//从键盘输入5个学生的成绩（整数），求他们的平均成绩（浮点数，保留一位小数）。
+//
+//输入描述:
+//一行，连续输入5个整数（范围0~100），用空格分隔。
+//输出描述:
+//一行，输出5个数的平均数（保留一位小数）。
+//示例1
+//输入
+//75 80 43 67 96
+//输出
+//72.2
+//方法一
+#include<stdio.h>
+int main()
+{
+    int i=0;
+    int sum=0;
+    for(i=0;i<5;i++)
+    {
+        int score=0;
+        scanf("%d",&score);
+        sum+=score;
+    }
+    printf("%.1f\n",sum/5.0);
+    return 0;
+}
+//方法二
+//#include<stdio.h>
+//int main()
+//{
+//    int i=0,sum=0;
+//    int score[5]={0};
+//    for(i=0;i<5;i++)
+//    {
+//        scanf("%d",&score[i]);
+//    }
+//    for(i=0;i<5;i++)
+//    {
+//        sum=sum+score[i];
+//    }
+//    printf("%.1f\n",sum/5.0);
+//    return 0;
+//}
