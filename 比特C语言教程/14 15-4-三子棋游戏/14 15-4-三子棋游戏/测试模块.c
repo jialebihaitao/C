6c7407abaaf83f355b52a5c42b3a1a88dc931fcf
加勒比海涛 <13555089837@163.com>
2021-01-18 12:45:54 +0800

    14/15-4-三子棋游戏

---
+++ 比特C语言教程/14 15-4-三子棋游戏/14 15-4-三子棋游戏/测试模块.c
@@ -0,0 +1,87 @@
+#define _CRT_SECURE_NO_WARNINGS
+//测试三子棋游戏
+#include<stdio.h>
+#include"game.h"
+#include"游戏模块.c"
+
+void menu()
+{
+	printf("******************************\n");
+	printf("******1.play    0.exit********\n"); 
+	printf("******************************\n");
+}
+
+//游戏的整个算法实现
+void game()
+{
+	char ret = 0;
+	//创建数组,存放棋盘信息
+	char board[ROW][COL] = { 0 };//希望数组内所有元素都是空格
+	//初始化棋盘
+	InitBoard(board,ROW,COL);
+	//打印棋盘
+	DisplayBoard(board,ROW,COL);
+	//下棋
+	while (1)
+	{
+		//玩家下棋
+		PlayerMove(board,ROW,COL);
+		DisplayBoard(board, ROW, COL);
+		//判断玩家输赢
+		ret=Iswin(board,ROW,COL);
+		if (ret != 'C')
+		{
+			break;
+		}
+		//电脑下棋
+		ComputerMove(board, ROW, COL);
+		DisplayBoard(board, ROW, COL);
+		//判断电脑输赢
+		ret = Iswin(board, ROW, COL);
+		if (ret != 'C')
+		{
+			break;
+		}
+	}
+	if (ret == '*')
+	{
+		printf("玩家赢\n");
+	}
+	else if (ret == '#')
+	{
+		printf("电脑赢\n");
+	}
+	else
+	{
+		printf("平局\n");
+	}
+}
+void test()
+{
+	int input = 0;
+	srand((unsigned int)time(NULL));
+	do
+	{
+		menu();
+
+		printf("请选择:>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			printf("三子棋\n");
+			break;
+		case 0:
+			printf("退出游戏\n");
+			break;
+		default:
+			printf("选择错误,请重新选择\n");
+			break;
+		}
+	} while (input);//while(input==0)
+}
+int main()
+{
+	test();
+	return 0;
+}
